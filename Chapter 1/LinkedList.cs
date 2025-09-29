321c26546fc34711cdeef9a070f8f4bea46cdb43
Eric Lippert <ericlippert@gmail.com>
2025-09-29 11:00:18 -0700

    Chapter one source code

---
+++ Chapter 1/LinkedList.cs
@@ -0,0 +1,38 @@
+﻿// Fabulous Adventures in Data Structures and Algorithms
+// Eric Lippert
+// Chapter 1
+
+// An immutable linked list
+
+// There are some good design decisions here but also some 
+// questionable ones!
+
+sealed record LinkedList<T>(T? Value, LinkedList<T>? Tail)
+{
+    public static LinkedList<T> Empty = new(default, null);
+    // C# infers "Empty = new LinkedList<T>(default(T), null)"
+
+    public LinkedList<T> Push(T value) => new(value, this);
+    public bool IsEmpty => this == Empty;
+    // Did you catch why this is correct but not as efficient
+    // as it could be? "IsEmpty => Tail == null" would be better.
+
+    public override string ToString()
+    {
+        var s = "";
+        for (var list = this; !list.IsEmpty; list = list.Tail!)
+            s += list.Value + " ";
+        // This is "accidentally quadradtic". A StringBuilder would
+        // be the better choice.
+        return s;
+    }
+    public LinkedList<T> Reverse()
+    {
+        var result = Empty;
+        for (var list = this; !list.IsEmpty; list = list.Tail!)
+            result = result.Push(list.Value!);
+        return result;
+    }
+}
+
+
---
+++ Chapter 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Fabulous Adventures in Data Structures and Algorithms
+// Eric Lippert
+// Chapter 1
+
+// An immutable linked list
+
+var list = LinkedList<int>.Empty.Push(10).Push(20).Push(30).Push(40).Push(50);
+var list2 = list.Reverse();
+// Reversing the list does not change the list.
+Console.WriteLine(list);
+// 50 40 30 20 10
+// 10 20 30 40 50
+Console.WriteLine(list2);
